62a10a03f5b24eec19b1dcacde930dc3dc7e1a86
ryandw11 <ryandw02@gmail.com>
2014-04-26 09:00:13 -0700

    Create code.py
    Created in python.

---
+++ code.py
@@ -0,0 +1,11 @@
+print ('Welcome to Ryan chat')
+print ('what is your user name?')
+user = input()
+print ('Welcome ' + user + ', to ryan chat!')
+usern = '[' + user + ']'
+e = 1
+r = 0
+while e > r:
+    e = e + 1
+    a = input()
+    print (usern + a)
